7b165b6f28ceba17f24c9c7604e4acd28f765187
Stephanie Marker <Stephanie.Marker93@gmail.com>
2018-07-30 11:38:16 -0400

    Initial cli commit.

---
+++ cli/blob_storage.py
@@ -0,0 +1,15 @@
+def set_blob_storage_service():
+    resource_group_name = context.obj['resource_group']
+    storage_account_name = context.obj['storage_account_name']
+    cached_key = (resource_group_name, storage_account_name)
+    blob_service = self._cached_blob_services.get(cached_key)
+
+    if blob_service is None:
+        with self._blob_services_lock:
+            blob_service = self._cached_blob_services.get(cached_key)
+            if blob_service is None
+                blob_service = BlockBlobService(account_name=storage_account_name,
+                                                account_key=context.obj['storage_account_key'])
+                self._cached_blob_services[cached_key] = blob_service
+    context.obj['blob_storage_service'] = blob_service
+    
---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+setup(
+    name="easycluster",
+    version='0.1',
+    py_modules=['hello'],
+    install_requires=[
+        'Click',
+        'azure',
+        'azure-storage-blob',
+        'azure-storage-file',
+        'colorama',
+        'azure-mgmt-batchai >= 2.0.0',
+        'python_version >= 3.6'
+    ],
+    entry_points='''
+        easycluster=easycluster:cli
+    '''
+)
