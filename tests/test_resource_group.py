20a6c52782284f4a8700ca44b8bb31af63c46b9a
rdc64 <chrobles@microsoft.com>
2018-08-03 22:50:00 -0400

    add library imports to tests to test for dependencies

--- tests/test_resource_group.py
+++ tests/test_resource_group.py
@@ -2,6 +2,7 @@ from hypothesis import given, example
 from hypothesis.strategies import fixed_dictionaries, text, from_regex, uuids
 
 from cli.regex import REGEX_DICT
+import cli.resource_group
 from cli.validation import validate_uuid, validate_rg_name, validate_location
 
 @example(fixed_dictionaries({}), text(), 'a2cfad07-90d4-4e4c-9227-095d90fcc7dd')
